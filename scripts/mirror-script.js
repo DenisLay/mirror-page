111db08d4e3a5a746f2726a8d0e68ef4b7d1ef87
DenisLay <56198059+DenisLay@users.noreply.github.com>
2020-09-17 20:50:51 +0300

    camera & video works

--- scripts/mirror-script.js
+++ scripts/mirror-script.js
@@ -1,1 +1,18 @@
-console.log('mirror');
+window.onload = function() {
+    var video = document.getElementById('video');
+    var videoStreamUrl = false;
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    window.URL.createObjectURL = window.URL.createObjectURL || window.URL.webkitCreateObjectURL || window.URL.mozCreateObjectURL || window.URL.msCreateObjectURL;
+
+    navigator.getUserMedia(
+        {video: true},
+        function(stream) {
+            videoStreamUrl = window.URL.createObjectURL(stream);
+            video.src = videoStreamUrl
+        },
+        function(){
+            alert('извините за мой французский, но вы пидор..');
+        }
+    )
+}
